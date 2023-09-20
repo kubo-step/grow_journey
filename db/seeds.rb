975baa0f9b30cbfe4ee66c98ab4979b469768c1e
yamabe mikako <xxx9.mm22rsq@gmail.com>
2023-09-20 16:52:57 +0900

    Add:入力フォームの項目追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Category.create!(name: '勉強')
+Category.create!(name: '仕事')
+Category.create!(name: '趣味')
+Category.create!(name: '健康')
+Category.create!(name: '美容')
+Category.create!(name: 'その他')
