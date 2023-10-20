c8f5407946ad9f9222109e0dea7e2cb4f395636e
yamabe mikako <xxx9.mm22rsq@gmail.com>
2023-10-20 15:05:41 +0900

    Fix:SSLのURL修正

--- config/puma.rb
+++ config/puma.rb
@@ -17,8 +17,8 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 #
 # port ENV.fetch("PORT") { 3000 }
 ssl_bind "0.0.0.0", "3000", {
-  cert: "config/cert/localhost.pem",
-  key: "config/cert/localhost-key.pem"
+  cert: "config/certs/localhost.pem",
+  key:  "config/certs/localhost-key.pem"
 }
 
 # Specifies the `environment` that Puma will run in.
