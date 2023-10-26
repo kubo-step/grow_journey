1c4fc5f578b66876060ff50ee6a202b28e32f67f
yamabe mikako <xxx9.mm22rsq@gmail.com>
2023-10-26 17:50:50 +0900

    Fix:プロフィール機能の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :goals, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 255 }
-  validates :line_user_id, presence: true
+  validates :line_user_id, presence: true, on: :edit
 
   mount_uploader :avatar, AvatarUploader
 end
