7e2e77903ea959eea546294bda46c0d3a5405320
yamabe mikako <xxx9.mm22rsq@gmail.com>
2023-10-28 23:17:09 +0900

    Fix:Userの情報修正

--- app/javascript/line_login.js
+++ app/javascript/line_login.js
@@ -1,41 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // csrf-tokenを取得
   const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
   const LIFF_ID = gon.liff_id;
 
   liff
-  .init({
-    liffId: LIFF_ID,
-    withLoginOnExternalBrowser: true
-  })
-    // 初期化後の処理の設定
+    .init({
+      liffId: LIFF_ID,
+      withLoginOnExternalBrowser: true
+    })
   liff
-  .ready.then(() => {
-    const idToken = liff.getIDToken()
-    // bodyにパラメーターの設定
-    const body =`idToken=${idToken}`
-    // リクエスト内容の定義
-    const request = new Request('/users', {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
-        'X-CSRF-Token': token
-      },
-      method: 'POST',
-      body: body
-    });
+    .ready.then(() => {
+      if (!liff.isLoggedIn()) {
+        liff.login();
+      } else {
+        liff.getProfile().then(profile => {
+          const idToken = liff.getIDToken()
+          const name = profile.displayName
+          const body = `idToken=${idToken}&name=${name}`
+          const request = new Request('/users', {
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+              'X-CSRF-Token': token
+            },
+            method: 'POST',
+            body: body
+          });
 
-    // リクエストを送る
-    fetch(request)
-    // jsonでレスポンスからデータを取得して/goalsに遷移する
-    .then(response => response.json())
-    .then(data => {
-      data_id = data
+          fetch(request)
+            .then(response => response.json())
+            .then(data => {
+              data_id = data
+            })
+            .then(() => {
+              window.location = '/goals'
+            })
+            .catch(error => {
+              console.error('エラー:', error);
+            });
+        })
+      }
     })
-    .then(() => {
-      window.location = '/goals'
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-  })
-});
+})
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_19_060006) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_28_140628) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,15 +36,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_19_060006) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", null: false
     t.string "name", null: false
     t.string "avatar"
-    t.string "crypted_password"
-    t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "line_user_id"
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.string "line_user_id", null: false
+    t.index ["line_user_id"], name: "index_users_on_line_user_id", unique: true
   end
 
   add_foreign_key "goals", "categories"
