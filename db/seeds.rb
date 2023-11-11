e118ff76119b7909d76d6836b51e200bfa9e3a4e
yamabe mikako <xxx9.mm22rsq@gmail.com>
2023-11-11 10:51:30 +0900

    Add:サンプルユーザーの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,47 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Category.create!(name: '勉強')
-Category.create!(name: '仕事')
-Category.create!(name: '趣味')
-Category.create!(name: '健康')
-Category.create!(name: '美容')
-Category.create!(name: 'その他')
+
+# ユーザーの作成
+user = User.find_or_create_by(email: "sample@example.com") do |u|
+  u.password = "sample"
+  u.password_confirmation = "sample"
+  u.provider = "guest_provider"
+  u.uid = "sample@example.com"
+  u.name = "サンプル"
+end
+
+# ユーザーが正しく作成されたかの確認
+if user.persisted?
+  puts "ユーザー #{user.email} が作成されました。"
+else
+  puts "ユーザーの作成に失敗しました: #{user.errors.full_messages.join(', ')}"
+end
+
+# カテゴリのサンプルデータを作成
+categories = %w[勉強 仕事 趣味 健康 美容 その他]
+categories.each do |category_name|
+  Category.find_or_create_by(name: category_name)
+end
+
+# 目標のサンプルデータを作成
+5.times do |i|
+  goal = user.goals.create(
+    content: "目標 #{i + 1}",
+    deadline: Time.zone.now + (i + 1).weeks,
+    category: Category.all.sample,
+    is_goal: true
+  )
+end
+
+# タスクのサンプルデータを作成
+5.times do |i|
+  task = user.goals.create(
+    content: "タスク #{i + 1}",
+    deadline: Time.zone.now + i.days,
+    category: Category.all.sample,
+    is_goal: false
+  )
+end
+
+puts "目標、タスクのサンプルデータを追加しました。"
