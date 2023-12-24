256ec350e4974e2f413f12af9880a093b84abe29
yamabe mikako <xxx9.mm22rsq@gmail.com>
2023-12-24 21:33:21 +0900

    Add:アイコン画像の追加

--- app/helpers/goals_helper.rb
+++ app/helpers/goals_helper.rb
@@ -1,2 +1,18 @@
 module GoalsHelper
+  def category_icon(category_name)
+    case category_name
+    when "勉強"
+      "icon/small_flower_yellow.png"
+    when "仕事"
+      "icon/small_flower_skyblue.png"
+    when "趣味"
+      "icon/small_flower_purple.png"
+    when "健康"
+      "icon/small_flower_red.png"
+    when "美容"
+      "icon/small_flower_pink.png"
+    else
+      "icon/small_flower_brown.png"
+    end
+  end
 end
