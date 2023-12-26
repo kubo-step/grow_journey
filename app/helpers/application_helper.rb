369b47cb10667ea92e47729087d615fe79fabd3d
yamabe mikako <xxx9.mm22rsq@gmail.com>
2023-12-26 10:15:31 +0900

    Fix:アイコンの種類変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,21 @@ module ApplicationHelper
     messages = "*#{field_name}#{message}"
     content_tag(:p, messages, class: "text-sm text-red-500")
   end
+
+  def category_icon(category_name)
+    case category_name
+    when "勉強"
+      "icon/small_flower_red.png"
+    when "仕事"
+      "icon/small_flower_yellow.png"
+    when "趣味"
+      "icon/small_flower_skyblue.png"
+    when "健康"
+      "icon/small_flower_orange.png"
+    when "美容"
+      "icon/small_flower_pink.png"
+    else
+      "icon/small_flower_brown.png"
+    end
+  end
 end
